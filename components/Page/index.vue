ffbb9ffa4b98eadc464856b9eb639c66bbbb77b0
daniele.falchetti <danielefalche@ibm.com>
2022-04-05 20:28:59 +0200

    first commit

---
+++ components/Page/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const props = defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div v-editable="blok">
+    <component
+      :is="element.component"
+      v-for="element in blok.body"
+      :key="element._id"
+      :blok="element"
+    />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useStoryApi, useStoryBridge } from "../services/stroyblok";
+const story = ref({});
+await useStoryApi()
+  .get("cdn/stories/home")
+  .then(({ data }) => (story.value = data.story));
+onMounted(() => useStoryBridge(story.id, (event) => (story = event)));
+</script>
+<template>
+  <div>
+    <Page :blok="story" />
+  </div>
+</template>
