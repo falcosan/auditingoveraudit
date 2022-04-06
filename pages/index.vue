30659bc9d8ffe1cd83d7fda5194447ca2751f48a
daniele.falchetti <danielefalche@ibm.com>
2022-04-06 08:52:14 +0200

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { useStoryApi, useStoryBridge } from "../services/stroyblok";
-const story = ref({});
-await useStoryApi()
-  .get("cdn/stories/home")
-  .then(({ data }) => (story.value = data.story));
-onMounted(() => useStoryBridge(story.id, (event) => (story = event)));
+const api = useStoryApi();
+const { data } = await api.get("cdn/stories/home");
+onMounted(() => {
+  useStoryBridge(state.story.id, (event) => {
+    state.story = event;
+  });
+});
+const state = reactive({
+  story: data.story,
+});
 </script>
 <template>
   <div>
-    <Page :blok="story" />
+    <Page :blok="state" />
   </div>
 </template>
